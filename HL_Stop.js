c3c786e2ad3cb61fc83bc6385539e62923acfbe9
krisstox <36812292+krisstox@users.noreply.github.com>
2018-04-28 09:29:48 +0200

    Update HL_Stop.js

--- HL_Stop.js
+++ HL_Stop.js
@@ -7,8 +7,7 @@ method.init = function() {
   this.addIndicator('zTrailingStop', 'zTrailingStop', this.settings.stoploss_threshold);
   
   //this.addTulipIndicator('ps', 'psar', {optInAcceleration: 0.25, optInMaximum: 0.50});
-  
-  
+    
   this.debug = false;
   this.Period = Math.round(this.settings.Period);
   this.candle_queue = [];
