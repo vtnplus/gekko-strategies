e62a75a283c336875a6c2431034f8d4cd3e6f123
krisstox <36812292+krisstox@users.noreply.github.com>
2018-05-01 14:10:53 +0200

    Update BB.js

--- indicators/BB.js
+++ indicators/BB.js
@@ -1,13 +1,8 @@
-// no required indicators
 // Bollinger Bands - Okibcn implementation 2018-01-02
 
 var Indicator = function(BBSettings) {
     this.input = 'price';
     this.settings = BBSettings; 
-    // Settings:
-    //    TimePeriod: The amount of samples used for the average.
-    //    NbDevUp: The distance in stdev of the upper band from the SMA.   
-    //    NbDevDn: The distance in stdev of the lower band from the SMA.   
     this.prices = [];
     this.diffs = [];
     this.age = 0;
@@ -16,9 +11,9 @@ var Indicator = function(BBSettings) {
     this.upper = 0;
     this.middle = 0;
     this.lower = 0;
-  }
-  
-  Indicator.prototype.update = function(price) {
+}
+
+Indicator.prototype.update = function(price) {
     var tail = this.prices[this.age] || 0; // oldest price in window
     var diffsTail = this.diffs[this.age] || 0; // oldest average in window
   
@@ -34,5 +29,5 @@ var Indicator = function(BBSettings) {
     this.lower = this.middle - this.settings.NbDevDn * stdev;
   
     this.age = (this.age + 1) % this.settings.TimePeriod
-  }
-  module.exports = Indicator;
+}
+module.exports = Indicator;
